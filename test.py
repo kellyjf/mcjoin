01efd53f04f8754675bf299973f0f6d1c380f5bb
John Kelly <jkelly@aircell.com>
2017-02-15 18:48:59 -0700

    Working example

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import socket
 
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.getprotobyname("udp"))
 
-s.connect(("225.2.3.4",5444))
-a1=socket.inet_pton(socket.AF_INET, "225.2.3.4")
+s.connect(("239.255.255.250",1900))
+a1=socket.inet_pton(socket.AF_INET, "239.255.255.250")
 a2=struct.pack("=I",0)
 aa=a1+a2
 b=struct.pack("i",5)
